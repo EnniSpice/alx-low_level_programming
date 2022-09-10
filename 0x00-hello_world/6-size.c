92d083ed6e01a02ee10c87292546a974464d1ebd
EnniSpice <sholatitus297@gmail.com>
2022-09-10 11:19:05 -0700

    6-size.c commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- print the stringin the put function
+ * Description: using the main function
+ * this programprints programming is like building a multilingual puzzle
+ * return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld bytes(s)\n", sizeof(i));
+	printf("size of a long int: %ld bytes(s)\n", sizeof(li));
+	printf("size of a long long int: %ld bytes(s)\n", sizeof(lli));
+	printf("size of a float: %ld bytes(s)\n", sizeof(f));
+	return (0):
+}
