c9dc53a3c0696c5be523aa7085659e93f4d324d7
EnniSpice <sholatitus297@gmail.com>
2022-09-10 01:59:24 -0700

    5-printf.c commit new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - print the string in the put function 
- * Description:  using the main function 
+ * main- print the string in the put function 
+ * Description: using the main function 
  * this program prints Programming is like building a multilingual puzzle
  * Return: 0 (display exactly what it was asked to)
  */
